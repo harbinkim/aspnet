30a63ef929c11a6da634497d6674044eb121e801
HarbinKim <harbik@uw.edu>
2017-04-20 19:48:47 -0700

    Initial commit

---
+++ BirthdayCard/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult BirthdayCardForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult BirthdayCardForm(Models.BirthdayCard birthdayResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("BirthdayCard", birthdayResponse);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
---
+++ BirthdayCard/Models/BirthdayCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayCard.Models
+{
+    public class BirthdayCard
+    {
+        [Required(ErrorMessage ="Please enter From")]
+        public string ToName { get; set; }
+        [Required(ErrorMessage = "Please enter To")]
+        public string FromName { get; set; }
+        [Required(ErrorMessage = "Please enter Message")]
+        public string Message { get; set; }
+
+    }
+}
